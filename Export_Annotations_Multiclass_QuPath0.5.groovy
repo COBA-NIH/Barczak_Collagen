21ce930ede77fc63612399218e6e4b0e6f36f1bf
Shatavisha Dasgupta <139376717+ShataDg@users.noreply.github.com>
2024-01-12 23:03:11 -0500

    Script for deriving and exporting pixels from multiclass image annotations - QuPath 0.5

---
+++ Export_Annotations_Multiclass_QuPath0.5.groovy
@@ -0,0 +1,24 @@
+// Deriving pixels from image annotations of multiple classes 
+
+def imageData = getCurrentImageData()
+
+// Define output path (relative to project)
+def outputDir = buildFilePath(PROJECT_BASE_DIR, 'export_fulllabeledimage')
+mkdirs(outputDir)
+def name = GeneralTools.stripExtension(imageData.getServer().getMetadata().getName())
+def path = buildFilePath(outputDir, name + "-labels.png")
+
+// Define how much to downsample during export (may be required for large images)
+//double downsample = 8
+
+// Create an ImageServer where the pixels are derived from annotations
+def labelServer = new LabeledImageServer.Builder(imageData)
+  .backgroundLabel(0, ColorTools.WHITE) // Specify background label (usually 0 or 255)
+  .addLabel('Tissue', 1)      // Choose output labels (the order matters!)
+  .addLabel('Collagen', 2)
+  .addLabel('Airspaces/Background', 3)
+  .multichannelOutput(false) // If true, each label refers to the channel of a multichannel binary image (required for multiclass probability)
+  .build()
+
+// Write the image
+writeImage(labelServer, path)
