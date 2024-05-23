79d6f2fd4cd782844f8c245edf8918ccb4b59897
Shatavisha Dasgupta <139376717+ShataDg@users.noreply.github.com>
2024-05-23 09:25:11 -0400

    added new script - export collagen annotations only

--- QuPath_scripts/Export_annotations_from_pixelclassifier_singleclass.groovy
+++ QuPath_scripts/Export_annotations_from_pixelclassifier_singleclass.groovy
@@ -1,8 +1,8 @@
 // Use pixel classifier to create annotations
 
 def minArea = 0.0 // To change
-def minHoleArea = 5.0 // To change
-def classifierName = "three_class_high_retrained" // Change to the name of the pixel classifier
+def minHoleArea = 2.0 // To change
+def classifierName = "new_classifier_set2" // Change to the name of the pixel classifier
 
 // Select all annotations
 selectAnnotations()
@@ -21,10 +21,10 @@ getAnnotationObjects().forEach {
 def imageData = getCurrentImageData()
 
 // Define output path (relative to project)
-def outputDir = buildFilePath(PROJECT_BASE_DIR, 'export_fulllabeledimage')
+def outputDir = buildFilePath(PROJECT_BASE_DIR, 'export_collagenlabeledimage')
 mkdirs(outputDir)
 def name = GeneralTools.stripExtension(imageData.getServer().getMetadata().getName())
-def path = buildFilePath(outputDir, name + "-labels.png")
+def path = buildFilePath(outputDir, name + "-collagen_labels.png")
 
 // Define how much to downsample during export (may be required for large images)
 //double downsample = 4
@@ -32,9 +32,7 @@ def path = buildFilePath(outputDir, name + "-labels.png")
 // Create an ImageServer where the pixels are derived from annotations
 def labelServer = new LabeledImageServer.Builder(imageData)
   .backgroundLabel(0, ColorTools.WHITE) // Specify background label (usually 0 or 255)
-  .addLabel('Tissue', 1)      // Choose output labels (the order matters!)
-  .addLabel('Collagen', 2)
-  .addLabel('Air', 3)
+  .addLabel('Collagen', 1)      // Choose output labels (the order matters!)
   .multichannelOutput(false) // If true, each label refers to the channel of a multichannel binary image (required for multiclass probability)
   .build()
 
