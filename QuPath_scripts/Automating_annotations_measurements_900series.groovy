0a26925af81affd292f687bfc6cbdfab9f91287c
Shatavisha Dasgupta <139376717+ShataDg@users.noreply.github.com>
2024-05-25 14:37:07 -0400

    added script for 900 series

---
+++ QuPath_scripts/Automating_annotations_measurements_900series.groovy
@@ -0,0 +1,19 @@
+//Script to automate creating annotations based on a pretrained pixel classifier on WSIs or tiles created from WSIs, and then measuring and exporting the areas, perimeters, and X and Y centroids of these annotations
+
+def minArea = 0.0 // To change
+def minHoleArea = 10.0 // To change
+def classifierName = "three_class_high_retrained" // Change to the name of the pixel classifier
+
+// Select all annotations
+selectAnnotations()
+
+// Apply pixel classifier inside them
+// Note: you can change this to createDetectionsFromPixelClassifier(..) if needed
+createAnnotationsFromPixelClassifier(classifierName, minArea, minHoleArea)
+
+// You could also filter out the small area that might be irrelevant?
+getAnnotationObjects().forEach {
+    print it.getROI().getArea()
+}
+
+print "Done!"
